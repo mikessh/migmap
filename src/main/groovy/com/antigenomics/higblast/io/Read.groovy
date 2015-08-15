5a1424b9efda69d8ae9d5efa07dd457e14605758
mikesh <mikhail.shugay@gmail.com>
2015-08-15 20:46:24 +0200

    Read quality

--- src/main/groovy/com/antigenomics/higblast/io/Read.groovy
+++ src/main/groovy/com/antigenomics/higblast/io/Read.groovy
@@ -1,20 +1,21 @@
+/*
+ * Copyright 2013-2015 Mikhail Shugay (mikhail.shugay@gmail.com)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.antigenomics.higblast.io
 
-/**
- Copyright 2014 Mikhail Shugay (mikhail.shugay@gmail.com)
-
- Licensed under the Apache License, Version 2.0 (the "License");
- you may not use this file except in compliance with the License.
- You may obtain a copy of the License at
-
- http://www.apache.org/licenses/LICENSE-2.0
-
- Unless required by applicable law or agreed to in writing, software
- distributed under the License is distributed on an "AS IS" BASIS,
- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- See the License for the specific language governing permissions and
- limitations under the License.
- */
 class Read {
     final String header, seq, qual
 
@@ -22,6 +23,10 @@ class Read {
         this.header = header
         this.seq = seq
         this.qual = qual
+    }
+
+    byte qualAt(int pos) {
+        qual ? ((int) (qual.charAt(pos)) - 33) : 40
     }
 
     @Override
