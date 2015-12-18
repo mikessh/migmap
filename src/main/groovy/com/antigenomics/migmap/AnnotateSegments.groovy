ac91279db985d64321739abe419ef75e1182283e
mikessh <mikhail.shugay@gmail.com>
2015-12-18 06:54:58 +0300

    Annotation of custom segments file

---
+++ src/main/groovy/com/antigenomics/migmap/AnnotateSegments.groovy
@@ -0,0 +1,204 @@
+/*
+ * Copyright (c) 2015, Bolotin Dmitry, Chudakov Dmitry, Shugay Mikhail
+ * (here and after addressed as Inventors)
+ * All Rights Reserved
+ *
+ * Permission to use, copy, modify and distribute any part of this program for
+ * educational, research and non-profit purposes, by non-profit institutions
+ * only, without fee, and without a written agreement is hereby granted,
+ * provided that the above copyright notice, this paragraph and the following
+ * three paragraphs appear in all copies.
+ *
+ * Those desiring to incorporate this work into commercial products or use for
+ * commercial purposes should contact the Inventors using one of the following
+ * email addresses: chudakovdm@mail.ru, chudakovdm@gmail.com
+ *
+ * IN NO EVENT SHALL THE INVENTORS BE LIABLE TO ANY PARTY FOR DIRECT, INDIRECT,
+ * SPECIAL, INCIDENTAL, OR CONSEQUENTIAL DAMAGES, INCLUDING LOST PROFITS,
+ * ARISING OUT OF THE USE OF THIS SOFTWARE, EVEN IF THE INVENTORS HAS BEEN
+ * ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ * THE SOFTWARE PROVIDED HEREIN IS ON AN "AS IS" BASIS, AND THE INVENTORS HAS
+ * NO OBLIGATION TO PROVIDE MAINTENANCE, SUPPORT, UPDATES, ENHANCEMENTS, OR
+ * MODIFICATIONS. THE INVENTORS MAKES NO REPRESENTATIONS AND EXTENDS NO
+ * WARRANTIES OF ANY KIND, EITHER IMPLIED OR EXPRESS, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY OR FITNESS FOR A
+ * PARTICULAR PURPOSE, OR THAT THE USE OF THE SOFTWARE WILL NOT INFRINGE ANY
+ * PATENT, TRADEMARK OR OTHER RIGHTS.
+ */
+
+package com.antigenomics.migmap
+
+import com.antigenomics.migmap.blast.BlastInstanceFactory
+import com.antigenomics.migmap.io.Read
+
+import java.util.regex.Pattern
+
+def ALLOWED_CHAINS = ["TRA", "TRB", "TRG", "TRG", "IGH", "IGL", "IGK"],
+    ALLOWED_SPECIES = ["human", "mouse", "rat", "rabbit", "rhesus_monkey"],
+    HOME = new File(this.class.protectionDomain.codeSource.location.path).parent.replaceAll("%20", " ")
+
+def cli = new CliBuilder(usage: "AnnotateSegments input.txt output.txt")
+
+// Runtime
+cli._(longOpt: "blast-dir", args: 1, argName: "path",
+        "Path to folder that contains 'igblastn' and 'makeblastdb' binaries. " +
+                "[default = assume they are added to \$PATH and execute them directly]")
+cli._(longOpt: "data-dir", args: 1, argName: "path",
+        "Path to folder that contains data bundle (internal_data/ and optional_file/ directories). " +
+                "[default = \$install_dir/data/]")
+cli.p(args: 1, argName: "int",
+        "Number of cores to use. [default = all available processors]")
+
+// Mapping
+cli.R(args: 1, argName: "chain1,...",
+        "Receptor gene and chain. Several chains can be specified, separated with commas. " +
+                "Allowed values: $ALLOWED_CHAINS. [required]")
+cli.S(args: 1, argName: "name",
+        "Species. Allowed values: $ALLOWED_SPECIES. [required]")
+cli._(longOpt: "use-kabat",
+        "Will use KABAT nomenclature for CDR/FW partitioning. " +
+                "[default = use IMGT nomenclature]")
+
+// Misc
+cli.h("Display this help message")
+
+// PARSE ARGUMENTS
+
+def opt = cli.parse(args)
+
+if (opt.h || opt == null || opt.arguments().size() != 2 || !opt.R || !opt.S) {
+    cli.usage()
+    System.exit(3)
+}
+
+// I/O
+
+def inputFileName = opt.arguments()[0], outputFileName = opt.arguments()[1]
+
+if (!new File(inputFileName).exists()) {
+    Util.error("Input file $inputFileName does not exist.", 3)
+}
+
+// Ensure all subdirs are created for output
+def parentFolder = new File(outputFileName).absoluteFile.parentFile
+if (parentFolder) {
+    parentFolder.mkdirs()
+}
+
+// Runtime
+
+if (opt.'blast-dir') {
+    ExecutionUtil.BLAST_HOME = new File((String) opt.'blast-dir').absolutePath
+}
+
+ExecutionUtil.checkBlastBinaries()
+
+def dataDir = new File((String) (opt.'data-dir' ?: "$HOME/data/")).absolutePath
+
+["internal_data/", "optional_file/"].each {
+    if (!new File("$dataDir/$it").exists()) {
+        Util.error("Internal data folder $it missing in $dataDir.", 3)
+    }
+}
+
+// Mapping
+
+def species = (String) opt.S, genes = ((String) opt.R).split(",") as List<String>,
+    useKabat = (boolean) opt.'use-kabat'
+
+if (!ALLOWED_SPECIES.contains(species)) {
+    Util.error("Unknown species $species.", 3)
+}
+
+boolean tr = false, ig = false
+genes.each { gene ->
+    if (!ALLOWED_CHAINS.contains(gene)) {
+        Util.error("Unknown gene $gene.", 3)
+    }
+    if (gene.toUpperCase().startsWith("TR")) {
+        tr = true
+    }
+    if (gene.toUpperCase().startsWith("IG")) {
+        ig = true
+    }
+}
+
+if (tr && ig) {
+    Util.error("Mixing TCR and IG genes is not allowed.", 3)
+}
+
+// Annotate input table
+
+// Deal with V segments first
+
+def vReferencePoints = new HashMap<Integer, Integer>()
+
+def blastInstanceFactory = new BlastInstanceFactory(dataDir, species, genes, true, useKabat)
+
+def instance = blastInstanceFactory.create()
+
+def readThread = new Thread(new Runnable() {
+    @Override
+    void run() {
+        def readMapping
+        while ((readMapping = instance.take()) != null) {
+            if (readMapping.mapped && readMapping.mapping.regionMarkup.cdr3Start >= 0) {
+                vReferencePoints[readMapping.read.header.toInteger()] = readMapping.mapping.regionMarkup.cdr3Start + 3
+            }
+        }
+    }
+})
+
+readThread.start()
+
+def i = 0
+new File(inputFileName).splitEachLine("[\t ]+") { splitLine ->
+    if (!splitLine[0].startsWith("#")) {
+        def seq = splitLine[5], segmentTypeStr = splitLine[2]
+
+        if (segmentTypeStr.startsWith("V")) {
+            instance.put(new Read(i.toString(), seq))
+        }
+    }
+    i++
+}
+
+instance.put(null)
+
+readThread.join()
+
+// Read one more time & assign reference points
+
+def J_PATTERN = Pattern.compile("T(?:GG|T[TC])GG.{4}GG.")
+
+def getJRef = { String seq ->
+    def matcher = J_PATTERN.matcher(seq)
+    matcher.find() ? matcher.start() - 1 : -2
+}
+
+i = 0
+new File(outputFileName).withPrintWriter { pw ->
+    new File(inputFileName).splitEachLine("[\t ]+") { splitLine ->
+        def refPoint = -1
+
+        if (!splitLine[0].startsWith("#")) {
+            def seq = splitLine[5], segmentTypeStr = splitLine[2]
+
+            if (segmentTypeStr.startsWith("V")) {
+                refPoint = vReferencePoints[i] ?: -2
+            } else if (segmentTypeStr.startsWith("J")) {
+                refPoint = getJRef(seq)
+            }
+
+            if (refPoint > -2) {
+                splitLine[4] = refPoint.toString()
+            } else {
+                splitLine[0] = "#" + splitLine[0]
+            }
+        }
+
+        pw.println(splitLine.join("\t"))
+        i++
+    }
+}
