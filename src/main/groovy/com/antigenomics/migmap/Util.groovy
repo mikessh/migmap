45a6b451bbf23cdddd62bff497f99215d1e1beca
mikessh <mikhail.shugay@gmail.com>
2016-06-17 19:08:20 +0300

    Once more

--- src/main/groovy/com/antigenomics/migmap/Util.groovy
+++ src/main/groovy/com/antigenomics/migmap/Util.groovy
@@ -93,7 +93,7 @@ class Util {
     @CompileStatic
     private static InputStream resourceGetHelper(String fname) {
         println new File(".").absolutePath
-        println new File(".").listFiles().collect { File it -> it.name }
+        println new File("build/resources/").listFiles().collect { File it -> it.name }
         def stream = Util.class.classLoader.getResourceAsStream(fname)
         if (!stream) {
             return new File("build/resources/main/" + fname).exists() ?
